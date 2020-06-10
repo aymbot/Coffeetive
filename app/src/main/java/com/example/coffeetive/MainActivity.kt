35c86da8e750d9c55b2f0023be1d3e0c5f709101
aymbot <aymeric.hollaus@stud.fh-campuswien.ac.at>
2020-06-10 15:00:26 +0200

    improved navigation drawer

--- app/src/main/java/com/example/coffeetive/MainActivity.kt
+++ app/src/main/java/com/example/coffeetive/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.coffeetive
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.coffeetive.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +19,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        //drawerLayout = binding.drawerLayout
+
+        val toolbar = findViewById<Toolbar?>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        drawerLayout = binding.drawerLayout
     }
 }
